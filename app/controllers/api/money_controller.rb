01bc7a691133603ead8594afe1d8d2ee80e6c98e
mfjmori <mfjmori@gmail.com>
2019-05-23 14:00:10 +0900

    simple to api/money_controller

--- app/controllers/api/money_controller.rb
+++ app/controllers/api/money_controller.rb
@@ -4,27 +4,18 @@ class Api::MoneyController < ApplicationController
   require 'json'
   require 'date'
 
-  def index
-  end
-
   def show
     if params[:type] == "ticker"
       url = URI.parse("https://public.bitbank.cc/#{params[:money_abbreviation]}_jpy/ticker")
-      response_text = Net::HTTP.get(url)
-      response_hash = JSON.parse(response_text)
-      render json: response_hash
     elsif params[:type] == "previous_day" || params[:type] == "candlestick-day"
       this_year = Date.today.strftime("%Y")
       url = URI.parse("https://public.bitbank.cc/#{params[:money_abbreviation]}_jpy/candlestick/1day/#{this_year}")
-      response_text = Net::HTTP.get(url)
-      response_hash = JSON.parse(response_text)
-      render json: response_hash
     elsif params[:type] == "candlestick-min"
       date = Date.today.strftime("%Y%m%d")
       url = URI.parse("https://public.bitbank.cc/#{params[:money_abbreviation]}_jpy/candlestick/#{params[:candle_type]}/#{date}")
-      response_text = Net::HTTP.get(url)
-      response_hash = JSON.parse(response_text)
-      render json: response_hash
     end
+    response_text = Net::HTTP.get(url)
+    response_hash = JSON.parse(response_text)
+    render json: response_hash
   end
 end
