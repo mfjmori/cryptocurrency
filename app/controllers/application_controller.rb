3242417ada4bbcf0db72f7a8173c5ff463aa2132
mfjmori <mfjmori@gmail.com>
2019-06-20 15:56:15 +0900

    add basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :get_all_money
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
     def get_all_money
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :role])
+    end
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+        username == 'admin' && password == '1111'
+      end
     end
 end
