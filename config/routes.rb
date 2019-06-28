07a29c9640461831ac9e7615ab648531c85f84dc
mfjmori <mfjmori@gmail.com>
2019-06-28 13:58:12 +0900

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ Rails.application.routes.draw do
   root 'money#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :money
+  resources :users, only: :show do
+    resources :cash_assets, only: :show
+    resources :crypto_assets, only: :index
+  end
+  resources :money do
+    resources :buy_orders, only: [:new, :create]
+    resources :sell_orders, only: [:new, :create]
+  end
   namespace :api do
     resources :money, only: [:index, :show], defaults: { format: 'json' }
   end
