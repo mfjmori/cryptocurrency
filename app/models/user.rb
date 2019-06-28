901eb37e88342b655df91e0a76febbcf70eda38c
mfjmori <mfjmori@gmail.com>
2019-06-28 13:54:51 +0900

    add association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :cash_asset
+  has_many :crypto_assets
   enum role: { normal: 0, admin: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
