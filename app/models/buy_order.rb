3cf16da0ca3a9c700fe447ea1c3c67e2f2d692a4
mfjmori <mfjmori@gmail.com>
2019-06-28 13:55:06 +0900

    add validation

--- app/models/buy_order.rb
+++ app/models/buy_order.rb
@@ -1,3 +1,14 @@
 class BuyOrder < ApplicationRecord
   belongs_to :crypto_asset
+
+  validates :number, presence: true, numericality: {greater_than: 0}
+  validates :value, presence: true, numericality: {only_integer: true, greater_than: 0}
+  
+  validate :value_must_be_less_than_cash, on: :create
+
+  def value_must_be_less_than_cash
+    if self.crypto_asset.user.cash_asset.number < value
+      errors.add(:value, "must be less than cash")
+    end
+  end
 end
--- app/models/crypto_asset.rb
+++ app/models/crypto_asset.rb
@@ -3,4 +3,8 @@ class CryptoAsset < ApplicationRecord
   belongs_to :money
   has_many :buy_orders
   has_many :sell_orders
+
+  validates :number, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :payed_cash, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
 end
