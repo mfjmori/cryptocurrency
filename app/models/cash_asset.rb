fd6b9a9199bd7b80b56aa7be5448cffcb3bdf37b
mfjmori <mfjmori@gmail.com>
2019-07-03 19:42:45 +0900

    update to create cash_asset when user sign_up

--- app/models/cash_asset.rb
+++ app/models/cash_asset.rb
@@ -2,5 +2,6 @@ class CashAsset < ApplicationRecord
   belongs_to :user
   belongs_to :money
 
+  validates :user_id, presence: true
   validates :number, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_one :cash_asset
-  has_many :crypto_assets
+  has_one :cash_asset, dependent: :destroy
+  has_many :crypto_assets, dependent: :destroy
   enum role: { normal: 0, admin: 1 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :role, numericality: {only_integer: true, less_than_or_equal_to: 1}
+  validates :role,
+    inclusion: {in: User.roles.keys}
+  validates :nickname, presence: true, length: { in: 1..10 }
 end
