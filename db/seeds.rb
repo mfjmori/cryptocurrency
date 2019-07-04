3f7130178354c52999e324c7dc483f4e13847034
mfjmori <mfjmori@gmail.com>
2019-07-04 20:46:41 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,12 @@
 #   nickname: 'admin',
 #   role: 1
 # )
-cash_asset = CashAsset.find_or_initialize_by(user_id: 2)
-cash_asset.number = 10000000
-cash_asset.money_id = 1
-cash_asset.save!
+users = User.all
+users.each do |user|
+  cash_asset = CashAsset.find_or_initialize_by(user_id: user.id)
+  money = Money.find_by(abbreviation: "jpy")
+  cash_asset.number = 10000000
+  cash_asset.money_id = money.id
+  cash_asset.save!
+end
 
