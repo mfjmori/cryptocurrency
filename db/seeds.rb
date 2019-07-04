07badb374afd591b8bb505c478dd1f249d7311c8
mfjmori <mfjmori@gmail.com>
2019-07-04 20:32:34 +0900

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-User.create!(
-  email: 'admin@gmail.com',
-  password: '10s3029h',
-  nickname: 'admin',
-  role: 1
-)
+# User.create!(
+#   email: 'admin@gmail.com',
+#   password: '10s3029h',
+#   nickname: 'admin',
+#   role: 1
+# )
+cash_asset = CashAsset.find_or_initialize_by(user_id: 2)
+cash_asset.number = 10000000
+cash_asset.save!
+
