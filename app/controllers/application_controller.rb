8b9f65bcb737fbf8bd25f08845b5ad8286f9f673
mfjmori <mfjmori@gmail.com>
2019-07-09 22:10:24 +0900

    add production method application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,10 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :role])
     end
 
+    def production?
+      Rails.env.production?
+    end
+
     def basic_auth
       authenticate_or_request_with_http_basic do |username, password|
         username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
