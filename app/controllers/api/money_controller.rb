72fa1df53b2c5b875b9bff481c46bb716a920217
mfjmori <mfjmori@gmail.com>
2019-07-13 11:16:55 +0900

    simplify candlestick

--- app/controllers/api/money_controller.rb
+++ app/controllers/api/money_controller.rb
@@ -3,16 +3,15 @@ class Api::MoneyController < ApplicationController
     if params[:type] == "ticker"
       url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/ticker")
     elsif params[:type] == "previous_day"
-      this_year = Date.today.strftime("%Y")
-      url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/candlestick/1day/#{this_year}")
+      ymd = Date.today.strftime("%Y")
+      url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/candlestick/1day/#{ymd}")
     elsif params[:type] == "candlestick"
-      if params[:candle_type] == "day"
-        this_year = Date.today.strftime("%Y")
-        url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/candlestick/1day/#{this_year}")
-      else
-        date = Date.today.strftime("%Y%m%d")
-        url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/candlestick/#{params[:candle_type]}/#{date}")
+      if params[:candle_type] == "1day"
+        ymd = Date.today.strftime("%Y")
+      elsif params[:candle_type] == "1min" || params[:candle_type] == "5min"
+        ymd = Date.today.strftime("%Y%m%d")
       end
+      url = URI.parse("https://public.bitbank.cc/#{params[:id]}_jpy/candlestick/#{params[:candle_type]}/#{ymd}")
     end
     response_text = Net::HTTP.get(url)
     response_hash = JSON.parse(response_text)
